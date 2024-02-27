95df337a1e537352bdfa66ea6d65a231199362b5
sti-munene <kinyuamunene.km@gmail.com>
2024-02-27 13:37:37 +0300

    refactor: layout the 404 page

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,18 +1,37 @@
 import { Container } from "@/components/Container";
+import { GenericHead } from "@/components/GenericHead";
 import { Heading } from "@/components/typography";
-import React from "react";
+import { Button } from "@/components/ui/button";
+import { useRouter } from "next/router";
 
 function NotFoundPage() {
+  const router = useRouter();
+
   return (
-    <div>
-      <Container>
-        <div>
-          <Heading as="h4" size="heading6">
-            Resource not found
-          </Heading>
-        </div>
-      </Container>
-    </div>
+    <>
+      <GenericHead title="Resource not found" />
+
+      <div>
+        <Container>
+          <div
+            style={{
+              height: "calc(100vh - 84px)",
+            }}
+            className="w-full grid place-items-center"
+          >
+            <div className="flex flex-col items-center gap-2">
+              <Heading as="h4" size="heading6">
+                Resource not found
+              </Heading>
+
+              <Button onClick={() => router.back()} variant="outline">
+                Go Back
+              </Button>
+            </div>
+          </div>
+        </Container>
+      </div>
+    </>
   );
 }
 
